cbb6eeb8f9da57e54acc5bf0feb6ba99c1388b69
VladimirKoshovec <koshavladmir@gmail.com>
2024-05-30 21:33:49 +0300

    add arr

--- my-app/src/MyComponenet/MyComponenet.jsx
+++ my-app/src/MyComponenet/MyComponenet.jsx
@@ -1,9 +1,12 @@
+import items from "../items";
+
 const MyComponenet = ({ message }) => {
 
     return (<>
     <h2>My Componenet</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam cupiditate repellat fugit eligendi facere, vero veniam reiciendis quidem impedit deserunt animi sunt excepturi cumque quos consectetur non porro praesentium. Explicabo.</p>
     <p>{ message }</p>
+    { items.map((el)=> {return <h3 key = {el.id} > {el.name} </h3>} ) }
     </>)
 };
 
---
+++ my-app/src/items.js
@@ -0,0 +1,5 @@
+const items = [{"id":1,"name":"Alice"},
+{"id":2,"name":"Done"},
+{"id":3,"name":"Boby"}];
+
+export default items;

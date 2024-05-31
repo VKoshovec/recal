936a28a1cf02ed859f3ac78614a5fe7eb821c35b
VladimirKoshovec <koshavladmir@gmail.com>
2024-05-31 11:47:16 +0300

    add sass

--- my-app/src/MyComponenet/MyComponenet.jsx
+++ my-app/src/MyComponenet/MyComponenet.jsx
@@ -1,9 +1,10 @@
 import items from "../items";
+import scss from './MyComponenet.module.scss';
 
 const MyComponenet = ({ message }) => {
 
     return (<>
-    <h2>My Componenet</h2>
+    <h2 className={ scss.bodyText } >My Componenet</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam cupiditate repellat fugit eligendi facere, vero veniam reiciendis quidem impedit deserunt animi sunt excepturi cumque quos consectetur non porro praesentium. Explicabo.</p>
     <p>{ message }</p>
     { items.map((el)=> {return <h3 key = {el.id} > {el.name} </h3>} ) }

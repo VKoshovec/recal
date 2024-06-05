2feec537a424fa9cc7bf91e402b0ef8a1698ff0a
VladimirKoshovec <koshavladmir@gmail.com>
2024-06-05 22:35:04 +0300

    add_spin

--- my-app/src/Pokemons/Pokemons.jsx
+++ my-app/src/Pokemons/Pokemons.jsx
@@ -1,27 +1,36 @@
 import { useState, useEffect } from "react";
-import { pokemon, pokemonArr } from "../Api/api";
+import { pokemonArr } from "../Api/api";
+import { ColorRing } from 'react-loader-spinner';
+
 
 const Pokemons = () => {
 
     const [pokemons, addPokemons] = useState([]);
+    const [loader, setLoder] = useState(true);
 
     useEffect(()=>{
 
-      pokemonArr(40).then( 
-        resp => { console.log(resp);
-                  addPokemons(resp) }
-      )
+      pokemonArr(1000).then( 
+        resp => {addPokemons(resp); setLoder(false)})
        
-    },[])
-
-    
+    },[])  
 
     return (
-        <div>  
-           { pokemons.map((el)=>{
-             return <p>{el.name}</p>
-           }) } 
-        </div>
+        <>  {(loader === true) ? <ColorRing
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="color-ring-loading"
+            wrapperStyle={{}}
+            wrapperClass="color-ring-wrapper"
+            colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+            /> : 
+            <div>
+            <ul>  
+               { pokemons.map(({name}, number) =>{ return <li id={number} key={number}>{name}</li> })} 
+            </ul>
+            </div>}
+        </>
     )
 };
 
